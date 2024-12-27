8db51ef6c8d87683b37ada8378bb01b15683fc88
MLayMik <maximmostov2@gmail.com>
2024-12-27 17:21:27 +0100

    wip

--- src/pages/Admin/AdminPanel.tsx
+++ src/pages/Admin/AdminPanel.tsx
@@ -1,32 +1,68 @@
-import { type } from 'node:os'
-import { AddressCreate } from '@/features/address/create'
-import { useMeetings } from '@/shared/api/meetings'
+import { useDeleteFriendlyMeeting } from '@/shared/api/friendly-meeting'
+import { useDeleteMeeting, useMeetings } from '@/shared/api/meetings'
+import { useDeleteMinistryMeeting, useMinistryMeetings } from '@/shared/api/ministry-meeting'
+import { useDeleteService } from '@/shared/api/service'
 import { cn } from '@/shared/lib/styles'
-import { Dialog, RadioCards } from '@radix-ui/themes'
+import { RadioCards, Separator } from '@radix-ui/themes'
 import { Plus, X } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 
 export function AdminPanel() {
-  const { data, isLoading } = useMeetings()
-  if (data) {
-    const formattedDate = `${data[1].date.getFullYear()}-${String(data[1].date.getMonth() + 1).padStart(2, '0')}-${String(data[1].date.getDate()).padStart(2, '0')}`
-    console.log(formattedDate)
-  }
+  const { data: meetingData, isLoading } = useMeetings()
+  const { data: ministryData } = useMinistryMeetings()
+  const { mutate: deleteMeeting } = useDeleteMeeting()
+  const { mutate: deleteService } = useDeleteService()
+  const { mutate: deleteMinistryMeeting } = useDeleteMinistryMeeting()
+  const { mutate: deleteFriendly } = useDeleteFriendlyMeeting()
 
   const navigate = useNavigate()
-  if (isLoading) {
-    return (
-      <div className="mx-auto">
-        Loading...
-        <Spinner />
-      </div>
-    )
-  }
+  if (isLoading)
+    return <div>Loading...</div>
 
   return (
     <div className="p-6">
-      <div className="m-5">
-        {/* <Link to="/admin/meeting">Meeting</Link> */}
+      <div className="my-3 text-center font-bold">
+        Панель редактирования
+      </div>
+      <Separator className="w-full" />
+      <p className="my-4 font-bold">
+        Встречи Собрания:
+      </p>
+      <RadioCards.Root columns={{ initial: '2', sm: '3' }}>
+        {meetingData?.map(meeting => (
+          <button
+            onClick={() => navigate(`meeting/${meeting.id}`)}
+            className={cn(
+              `
+                relative h-20 rounded-md border bg-white px-4 py-2 text-start text-sm transition-all
+                duration-200 ease-in-out
+
+                dark:border-gray-600 dark:bg-transparent
+
+                hover:drop-shadow-mainshadow
+              `,
+            )}
+            key={meeting.id}
+          >
+            <button
+              onClick={(e) => {
+                e.stopPropagation()
+                deleteService({ id: meeting.serviceId! })
+                deleteMeeting({ id: meeting.id })
+              }}
+              className="absolute right-1 top-1 cursor-pointer"
+              role="button"
+              tabIndex={0}
+            >
+              <X className="size-3" />
+            </button>
+            <p className={meeting.status!.title !== 'Собрание' ? 'font-bold' : undefined}>{meeting.status!.title}</p>
+            <p className="text-xs">
+              {meeting.date.toLocaleString('ru', { day: 'numeric', month: 'long', year: 'numeric' })}
+            </p>
+          </button>
+        ))}
+
         <button
           onClick={() => navigate(`/admin/meeting`)}
           className={`
@@ -40,15 +76,16 @@ export function AdminPanel() {
         >
           <Plus />
         </button>
+
       </RadioCards.Root>
-
       <Separator className="my-4 w-full" />
-
-      <p className="mb-4 font-bold">Встречи для проповеди:</p>
+      <p className="mb-4 font-bold">
+        Встречи для проповеди:
+      </p>
       <RadioCards.Root columns={{ initial: '2', sm: '3' }}>
         {ministryData?.map(meeting => (
           <button
-            onClick={() => navigate(`/admin/ministry-meeting/${meeting.id}`)}
+            onClick={() => navigate(`ministry-meeting/${meeting.id}`)}
             className={cn(
               `
                 relative h-20 rounded-md border bg-white px-4 py-2 text-start text-sm transition-all
@@ -78,10 +115,11 @@ export function AdminPanel() {
             <p>
               {meeting.date.toLocaleString('ru', { day: 'numeric', month: 'long', year: 'numeric' })}
             </p>
-            <p className="text-xs">{meeting.leader}</p>
+            <p className="text-xs">
+              {meeting.leader}
+            </p>
           </button>
         ))}
-
         <button
           onClick={() => navigate(`/admin/ministry-meeting`)}
           className={`
