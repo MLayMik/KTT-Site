1deca5f0827bc31924ded54d918e69a08269000a
MLayMik <maximmostov2@gmail.com>
2025-01-08 13:04:56 +0100

    feat(admin): added require auth for admin routes

--- src/app/route/App.tsx
+++ src/app/route/App.tsx
@@ -1,4 +1,5 @@
 import { AdminMeeting, AdminMinistryMeetings, AdminPanel } from '@/pages/Admin'
+import { RequireAuth } from '@/shared/lib/utils/RequireAuth'
 import { Route, Routes } from 'react-router-dom'
 import { Announcements } from '../../pages/Announcements/Announcements'
 import { Areas } from '../../pages/Areas'
@@ -12,11 +13,27 @@ function App() {
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route index element={<MeetingProgram />} />
-        <Route path="admin" element={<AdminPanel />} />
-        <Route path="admin/meeting" element={<AdminMeeting />} />
-        <Route path="admin/meeting/:id" element={<AdminMeeting edit />} />
-        <Route path="admin/ministry-meeting" element={<AdminMinistryMeetings />} />
-        <Route path="admin/ministry-meeting/:id" element={<AdminMinistryMeetings edit />} />
+        <Route
+          path="admin"
+          element={(
+            <RequireAuth>
+              <AdminPanel />
+            </RequireAuth>
+          )}
+        />
+        <Route
+          path="admin/*"
+          element={(
+            <RequireAuth>
+              <Routes>
+                <Route path="meeting" element={<AdminMeeting />} />
+                <Route path="meeting/:id" element={<AdminMeeting edit />} />
+                <Route path="ministry-meeting" element={<AdminMinistryMeetings />} />
+                <Route path="ministry-meeting/:id" element={<AdminMinistryMeetings edit />} />
+              </Routes>
+            </RequireAuth>
+          )}
+        />
         <Route path="meeting-service" element={<MeetingService />} />
         <Route path="areas" element={<Areas />} />
         <Route path="announcements" element={<Announcements />} />
