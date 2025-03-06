19edb3972a8bc3dcd0e79475c831ec0cc7b169dc
Ваше Имя <maximmostov2@gmail.com>
2025-03-06 13:20:11 +0100

    test(formatDate): fix

--- src/shared/lib/utils/formatDate.test.tsx
+++ src/shared/lib/utils/formatDate.test.tsx
@@ -4,13 +4,13 @@ import { formatDate } from './formatDate'
 const date = new Date(2025, 0, 1, 14, 30, 0)
 
 it('returns formatted date', () => {
-  expect(formatDate(date)).toBe('1 января')
+  expect(formatDate({ date })).toBe('1 января')
 })
 
 it('returns formatted date with year', () => {
-  expect(formatDate(date, { showYear: true })).toBe('1 января 2025 г.')
+  expect(formatDate({ date, options: { showYear: true } })).toBe('1 января 2025 г.')
 })
 
 it('returns formatted date with numeric month', () => {
-  expect(formatDate(date, { numericMonth: true })).toBe('01.01')
+  expect(formatDate({ date, options: { numericMonth: true } })).toBe('01.01')
 })
